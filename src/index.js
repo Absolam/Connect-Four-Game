d4b458b66995a82a1bedeb1e59b15d90a7c83848
Aaron A <aaronabsolam@yahoo.com>
2022-10-30 19:39:48 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import { Menu } from "./components/menu/Menu";
 import { Rules } from "./components/rules/Rules";
 import { GameVsCPU } from "./components/game/vsCPU/GamevsCPU";
@@ -10,14 +10,14 @@ import { GameVsPlayer } from "./components/game/vsPlayer/GamevsPlayer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-      <Route path="/Connect-Four-Game" element={<App />}>
-        <Route path="/Connect-Four-Game" element={<Menu />} />
-        <Route path="/Connect-Four-Game/rules" element={<Rules />} />
-        <Route path="/Connect-Four-Game/vsCPU" element={<GameVsCPU />} />
-        <Route path="/Connect-Four-Game/vsPlayer" element={<GameVsPlayer />} />
+      <Route path="/" element={<App />}>
+        <Route path="/" element={<Menu />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/vsCPU" element={<GameVsCPU />} />
+        <Route path="/vsPlayer" element={<GameVsPlayer />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
