d6a900d3fed729e2c9b7507f3024a518068dd359
Aaron A <aaronabsolam@yahoo.com>
2022-10-31 12:55:15 -0400

    fixed win message

--- src/components/game/PlayerWon.jsx
+++ src/components/game/PlayerWon.jsx
@@ -4,7 +4,9 @@ export const PlayerWon = ({ win, reset }) => {
   return (
     <div className={styles.playerWon}>
       <p>{win.player === "TIE" ? "" : win.player.toUpperCase()}</p>
-      <p>{win.player === "TIE" ? "TIE" : "WINS"}</p>
+      <p>
+        {win.player === "TIE" ? "TIE" : win.player === "YOU" ? "WIN" : "WINS"}
+      </p>
       <button onClick={reset}>PLAY AGAIN</button>
     </div>
   );
